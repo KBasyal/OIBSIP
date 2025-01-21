dc9383225b4dec14298a68a3ab1b994c38f3800d
kbasyal <kamalbasyal987@gmial.com>
2025-01-21 21:41:28 +0545

    express config and routing update

--- backend/src/config/express.config.js
+++ backend/src/config/express.config.js
@@ -1,12 +1,17 @@
 const express = require('express')
-
 const app = express()
+
+const authRouter = require("../modules/auth/auth.router")
+const mainRouter = require("./routing.config")
+
+app.use(mainRouter)
+app.use(authRouter)
 
 app.use('/',(request, response)=>{
     // response.send("Hello guys are you still there?")
-    response.json({
+    response.status(404).json({
         result:"any",
-        message :"Success",
+        message :"Request not found",
         meta:null
     })
 })
---
+++ backend/src/modules/auth/auth.router.js
@@ -0,0 +1,8 @@
+const express = require("express");
+const authRoutes = express()
+
+authRoutes.use("/auth/register",(request, response)=>{
+
+})
+
+module.exports =authRoutes;
