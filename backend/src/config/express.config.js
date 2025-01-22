1ff6e60c67557095b867d80c00e196e6c4e5603c
kbasyal <kamalbasyal987@gmial.com>
2025-01-22 23:08:11 +0545

    uploader middleware and validator

--- backend/src/config/express.config.js
+++ backend/src/config/express.config.js
@@ -1,11 +1,11 @@
 const express = require('express')
 const app = express()
 
-const authRouter = require("../modules/auth/auth.router")
 const mainRouter = require("./routing.config")
 
 app.use(mainRouter)
-app.use(authRouter)
+
+
 
 app.use('/',(request, response)=>{
     // response.send("Hello guys are you still there?")
--- backend/src/config/routing.config.js
+++ backend/src/config/routing.config.js
@@ -1,5 +1,11 @@
-const express = require('express')
-const mainRoute = express()
+const mainRoute = require('express').Router()
+// importing routes form different modules
+const authRouter = require("../modules/auth/auth.router")
+
+// Attaching these routes to mainRouter
+mainRoute.use(authRouter)
+
+
 
 
 
