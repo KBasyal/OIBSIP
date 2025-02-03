8d4b6b0a6f998749e213535a27ef4e8d3505e223
kbasyal <kamalbasyal987@gmail.com>
2025-02-03 22:20:42 +0545

    adding user update

--- backend/src/config/routing.config.js
+++ backend/src/config/routing.config.js
@@ -2,10 +2,12 @@ const mainRoute = require('express').Router()
 // importing routes form different modules
 const authRoutes = require("../modules/auth/auth.router")
 const bannerRoutes = require("../modules/banner/banner.router")
+const userRoutes=require("../modules/user/user.router")
 
 
 // Attaching these routes to mainRouter
 mainRoute.use('/auth', authRoutes)
+mainRoute.use('/user',userRoutes)
 mainRoute.use('/banner', bannerRoutes)
 
 
---
+++ backend/src/modules/user/user.controller.js
@@ -0,0 +1,48 @@
+const userSvc = require("./user.service");
+
+class UserController {
+    index = async (req, res, next) => {
+        try {
+            const page = +req.query.page || 1;
+            const limit = +req.query.limit || 15;
+            // databse table has 1-100 id 
+            // per page 15
+            // 1-15 ===>page 1
+            const skip = (page - 1) * limit;
+            let filter = {};
+            if (req.query.search) {
+                filter = {
+                    name: new RegExp(req.query.search, 'i'),
+                    email: new RegExp(req.query.search, 'i')
+                }
+            }
+            if (req.query.role) {
+                filter = {
+                    ...filter,
+                    role: req.query.role
+                }
+            }
+            const data = await userSvc.listAll({
+                limit: limit,
+                skip: skip,
+                filter: filter
+            });
+            const countData = await userSvc.count({
+                filter: filter
+            })
+            res.json({
+                result: data,
+                message: " User List",
+                meta: {
+                    page: page,
+                    total: countData,
+                    limit: limit
+                }
+            })
+        } catch (exception) {
+            next(exception)
+        }
+    }
+}
+const userCtrl = new UserController()
+module.exports = userCtrl;
